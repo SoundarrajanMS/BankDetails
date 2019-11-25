f9ea688da85c8bae905c4dc78598d3b55518d24f
SoundarrajanMS <ms.soundar09@gmail.com>
2019-11-25 18:08:40 +0530

    inheritance

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,46 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+	@Override
+	public void depositamt(char depamount) {
+		System.out.println("deposit id is-"+depamount);
+	}
+	int f=15;
+	int g= 25;
+	int h=f+g;
+	private void abcd(int i,int e) {
+		System.out.println("integer values are-"+i+e);
+
+	}
+	public static void main(String[] args, int d) {
+		AxisBank b = new AxisBank();
+
+		b.depositamt('v');
+		b.fixed(300000);
+		b.saving(234565432);
+		b.abcd(11, d);
+		int sou = 100;
+		System.out.println("print-"+100);
+	int a=20;
+	int b1=10;
+	int c=a/b1;
+	
+	System.out.println("list-"+a);
+	System.out.println("resut-"+c);
+
+	
+	}
+	
+	
+	
+	
+
+		
+
+	
+			
+	
+
+	}
+	
+
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,24 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving(long sac) {
+		System.out.println("saving bank accno-"+sac);
+		}
+	
+	
+	public void fixed(int fxdamount) {
+		System.out.println("fixed deposit of the account is-Rs."+fxdamount);
+		
+
+	}
+	public void depositamt(char depamount) {
+		System.out.println("deposit id is-"+depamount);
+	
+
+	
+	}
+
+}
+	
+
+
